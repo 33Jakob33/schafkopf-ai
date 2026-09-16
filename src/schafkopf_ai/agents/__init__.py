77681832d9fd72c3f1b954323b8320b0e3fd2fd3
33Jakob33 <jakob.schleicher@web.de>
2026-09-16 02:19:56 +0200

    Export neural card play agent

--- src/schafkopf_ai/agents/__init__.py
+++ src/schafkopf_ai/agents/__init__.py
@@ -1,5 +1,6 @@
 from .agent import Agent
 from .heuristic_agent import HeuristicAgent, InferredVoids, infer_voids
+from .neural_card_play_agent import NeuralCardPlayAgent
 from .random_agent import RandomAgent
 from .strategic_heuristic_agent import StrategicHeuristicAgent
 
@@ -7,6 +8,7 @@ __all__ = [
     "Agent",
     "HeuristicAgent",
     "InferredVoids",
+    "NeuralCardPlayAgent",
     "RandomAgent",
     "StrategicHeuristicAgent",
     "infer_voids",
